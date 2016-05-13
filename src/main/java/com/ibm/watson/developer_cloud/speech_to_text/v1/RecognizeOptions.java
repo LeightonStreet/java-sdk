ce510767b51e5bb58414bdd3bf8d34ba59609232
He Li <jet.in.brain@gmail.com>
2016-05-13 11:42:00 -0400

    remove wrong check

--- src/main/java/com/ibm/watson/developer_cloud/speech_to_text/v1/RecognizeOptions.java
+++ src/main/java/com/ibm/watson/developer_cloud/speech_to_text/v1/RecognizeOptions.java
@@ -92,8 +92,8 @@ public class RecognizeOptions {
       Validator.isTrue(MediaType.parse(contentType) != null,
           "contentType is not a valid mime audio format. Valid formats start with 'audio/'");
 
-      Validator.isTrue(contentType.contains(HttpMediaType.AUDIO_RAW) && contentType.contains("rate"),
-          "When using PCM the audio rate should be specified.");
+//      Validator.isTrue(contentType.contains(HttpMediaType.AUDIO_RAW) && contentType.contains("rate"),
+//          "When using PCM the audio rate should be specified.");
 
       this.contentType = contentType;
       return this;
